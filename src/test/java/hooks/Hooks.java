0c9fe167af4b3c51ef2f181bddb7fb8119e73a26
Sezer Ercan <sezercan0617@gmail.com>
2022-09-03 11:59:17 +0300

    murattaninciler

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -17,6 +17,7 @@ public class Hooks {
     public void setUp() {
 
    //seni cok seviyorum
+        // soyleme ki eskimesin
         spec = new RequestSpecBuilder().setBaseUri("https://amazon.com").build();
 
     }
